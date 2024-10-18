4a3cd8a217c35eb79ce702a93d818118579901c0
wook <yho7955@naver.com>
2024-10-18 15:29:11 +0900

    fix: conflict 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import styled from 'styled-components';
-import { useState, useEffect } from 'react';
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
-=======
->>>>>>> 1861f649ba827c05579b89e005037b780482f620
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import routes from '@/router/routes';
 
